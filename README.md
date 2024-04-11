17f15cda4d6d5efdaf74fadd06910254f60a412d
Ayush Chopra <ayushchopra0@gmail.com>
2024-04-11 11:48:33 +0530

    configured and ready with boiler plate

---
+++ README.md
@@ -0,0 +1,4 @@
+All the commands used to setup this project:
+- `npm create vite@latest` -> this setups the vite project
+- `cd weTweetfrontend` 
+- `npm install`
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import WeTweetApp from './components/WeTweetApp';
+import Card from './components/PostCard';
+function App() {
+
+
+  return (
+    <>
+      <WeTweetApp/>
+      <Card/>
+    </>
+  )
+}
+
+export default App
